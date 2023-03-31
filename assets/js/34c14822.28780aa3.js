71abe887c1af08240f439faf58b730956533d292
Ramiro Torres <ramixalex@hotmail.com>
2023-03-31 03:32:27 +0200

    Initial commit

---
+++ assets/js/34c14822.28780aa3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_parrot_os=self.webpackChunkmy_parrot_os||[]).push([[9779],{291:s=>{s.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"configure-parrot"}')}}]);
